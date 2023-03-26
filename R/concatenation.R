12653f7ff88df6dec99fecfebf0e5a637b2f126f
Bradley Setzler <setzler@users.noreply.github.com>
2023-03-26 18:25:12 -0400

    Switch to slash signs for horizontal concatenation
    This is to resolve Issue #2.

--- R/concatenation.R
+++ R/concatenation.R
@@ -17,38 +17,82 @@ print.tt_block <- function(x, ...) {
 }
 
 #' Concatenate textab blocks horizontally (side-by-side).
-#' @param top_row The top tabular row.
-#' @param bottom_row The bottom tabular row.
+#' @param left_row The left tabular row.
+#' @param right_row The right tabular row.
 #' @return A block of a tabular, formed by horizontally concatenating the two provided blocks.
-#' @export -.tt_
+#' @examples
+#' # define some textab blocks
+#' first_block = TexRow(c(1,2))
+#' first_block
+#'
+#' second_block = TexRow(3)
+#' second_block
+#'
+#' third_block = TexRow(4)
+#' third_block
+#'
+#' # concatenate two blocks horizontally
+#' first_block / second_block
+#'
+#' # concatenate three blocks horizontally
+#' first_block / second_block / third_block
+#'
+#' # concatenate both horizontally and vertically
+#' # note: horizontal concatenation takes precedence over vertical concatenation
+#' first_block + second_block / third_block
+#'
+#' @export /.tt_
 #' @export
-`-.tt_` <- function(top_row, bottom_row) {
-  if (("tt_block" %in% class(top_row)) && ("tt_block" %in% class(bottom_row))) {
+`/.tt_` <- function(left_row, right_row) {
+  if (("tt_block" %in% class(left_row)) && ("tt_block" %in% class(right_row))) {
     block <- tt_block()
-    block$row_list[[1]] <- c(top_row$row_list[[1]], bottom_row$row_list[[1]])
-    block$row_ending <- bottom_row$row_ending
-    block$ncol <- top_row$ncol + bottom_row$ncol
-    block$nrow <- top_row$nrow
+    block$row_list[[1]] <- c(left_row$row_list[[1]], right_row$row_list[[1]])
+    block$row_ending <- right_row$row_ending
+    block$ncol <- left_row$ncol + right_row$ncol
+    block$nrow <- left_row$nrow
     return(block)
   }
 }
 
+
+
 #' Concatenate textab blocks vertically.
-#' @param left_block The left block of the tabular row.
-#' @param right_block The right block of the tabular row.
+#' @param upper_block The upper block of the tabular row.
+#' @param lower_block The lower block of the tabular row.
 #' @return A block of a tabular, formed by vertically concatenating the two provided blocks.
+#' @examples
+#' # define some textab blocks
+#' first_block = TexRow(c(1,2))
+#' first_block
+#'
+#' second_block = TexRow(3)
+#' second_block
+#'
+#' third_block = TexRow(4)
+#' third_block
+#'
+#' # concatenate two blocks vertically
+#' first_block + second_block
+#'
+#' # concatenate three blocks vertically
+#' first_block + second_block + third_block
+#'
+#' # concatenate both horizontally and vertically
+#' # note: horizontal concatenation takes precedence over vertical concatenation
+#' first_block + second_block / third_block
+#'
 #' @export +.tt_
 #' @export
-`+.tt_` <- function(left_block, right_block) {
-  if (("tt_block" %in% class(left_block)) && ("tt_block" %in% class(right_block))) {
-    left_block$ncol <- pmax(left_block$ncol, right_block$ncol)
-    left_block$row_list <- c(left_block$row_list, right_block$row_list)
-    left_block$row_ending <- c(left_block$row_ending, right_block$row_ending)
-    left_block$nrow <- left_block$nrow + right_block$nrow
-    return(left_block)
+`+.tt_` <- function(upper_block, lower_block) {
+  if (("tt_block" %in% class(upper_block)) && ("tt_block" %in% class(lower_block))) {
+    upper_block$ncol <- pmax(upper_block$ncol, lower_block$ncol)
+    upper_block$row_list <- c(upper_block$row_list, lower_block$row_list)
+    upper_block$row_ending <- c(upper_block$row_ending, lower_block$row_ending)
+    upper_block$nrow <- upper_block$nrow + lower_block$nrow
+    return(upper_block)
   }
-  if (("tt_block" %in% class(left_block)) && ("tt_mod_ending" %in% class(right_block))) {
-    left_block$row_ending[[length(left_block$row_ending)]] <- right_block$str
-    return(left_block)
+  if (("tt_block" %in% class(upper_block)) && ("tt_mod_ending" %in% class(lower_block))) {
+    upper_block$row_ending[[length(upper_block$row_ending)]] <- lower_block$str
+    return(upper_block)
   }
 }
